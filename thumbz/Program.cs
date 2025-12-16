47424ac08edc0cda6436673972321f38d0c86266
Joseph Choi <jsk.choi@gmail.com>
2025-12-15 18:01:36 -0800

    don't clean orphan

--- thumbz/Program.cs
+++ thumbz/Program.cs
@@ -24,7 +24,7 @@ foreach (string path in args)
 {
     if (Directory.Exists(path))
     {
-        CleanOrphanedThumbnails(path);
+        // CleanOrphanedThumbnails(path);
         await ProcessDirectory(path);
     }
     else
